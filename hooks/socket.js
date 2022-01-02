096371a1005535a65fc8abcccc2b3e8352488aa3
Axar Tiberius <axar.tiberius@protonmail.com>
2022-01-01 16:17:28 -0800

    using nlp for responses

--- hooks/socket.js
+++ hooks/socket.js
@@ -1,6 +1,8 @@
 const socketio = require('socket.io')
 const ss = require('socket.io-stream')
 const speech = require('microsoft-cognitiveservices-speech-sdk');
+const containerBootstrap = require('@nlpjs/core-loader').containerBootstrap
+const Nlp = require('@nlpjs/nlp').Nlp
 
 module.exports = function container (get, set) {
   var speechConfig = speech.SpeechConfig.fromSubscription(get('conf.azure_key'), get('conf.azure_location'));
@@ -10,6 +12,19 @@ module.exports = function container (get, set) {
   speechConfig.speechSynthesisOutputFormat = speech.SpeechSynthesisOutputFormat.Audio24Khz160KBitRateMonoMp3
 
   return function task (cb) {
+    var nlp;
+    ;(new Promise(async (resolve, reject) => {
+      try {
+        const container = await containerBootstrap()
+        nlp = new Nlp({ container })
+        await nlp.load('data/model.nlp')
+        resolve()
+      } catch (err) {
+        //reject({ type: 'error', obj: err })
+        throw err
+      }
+    }));
+
     var srv = get('site.server')
     var io = process.env.WHTF_NODE_ENV === 'development'
       ? socketio(srv, { cors: { origin: `${process.env.WHTF_HOST}:3000` } })
@@ -26,22 +41,28 @@ module.exports = function container (get, set) {
           var synthesizer = new speech.SpeechSynthesizer(speechConfig);
           socket.emit('is-typing', true)
 
-          synthesizer.speakTextAsync(
-            data.value,
-            function (result) {
-              if (result) {
-                synthesizer.close()
-                var audioData = result.audioData
-                socket.emit('audio-forwarded', {buffer: Buffer.from(audioData)})
+          ;(async () => {
+            const response = await nlp.process('en', data.value);
+            socket.emit('answer', response.answer)
+            synthesizer.speakTextAsync(
+              response.answer,
+              function (result) {
+                if (result) {
+                  synthesizer.close()
+                  var audioData = result.audioData
+                  socket.emit('audio-forwarded', {buffer: Buffer.from(audioData)})
+                }
+                socket.emit('is-typing', false)
+              },
+              function (error) {
+                console.log(error);
+                socket.emit('is-typing', false)
+                synthesizer.close();
               }
-              socket.emit('is-typing', false)
-            },
-            function (error) {
-              console.log(error);
-              socket.emit('is-typing', false)
-              synthesizer.close();
-            }
-          );
+            );
+          })();
+
+          
 
           //log.info(`value: ${data.value}`)
         })
