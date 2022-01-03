7d724b5f66ed5e443cf6bd89134fe0528fcb578f
Axar Tiberius <axar.tiberius@protonmail.com>
2022-01-02 21:21:30 -0800

    unix line endings

--- middleware/cors.js
+++ middleware/cors.js
@@ -9,12 +9,14 @@ module.exports = function container (get, set) {
       )
     }
     // Allow several headers for our requests
+    /*
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept'
     )
+    */
 
-    res.header('Access-Control-Allow-Credentials', true)
+    //res.header('Access-Control-Allow-Credentials', true)
     next()
   }
 }
